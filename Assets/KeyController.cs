66aa781f9559a95a9e0c6be89072c509f8448ec4
Gaeus Caskie <fabrogaeuscaskie@gmail.com>
2022-12-16 15:55:21 +0700

    First commit
    Features
    *2D-Maze Scene
     -> 2D Maze
    *Player
    *Additional elements
     -> Floor
     -> Obstacles (animals, etc.)

---
+++ Assets/KeyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.tag=="Player")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static string pickUpKey = "n";
+    public static string haveFriend = "n";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Adding magnitude with direction 
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal")*4, Input.GetAxis("Vertical")*4);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag=="Key")
+        {
+            pickUpKey = "y"; 
+        }
+    }
+}
